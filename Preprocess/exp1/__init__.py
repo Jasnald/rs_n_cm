8427280a051333cd695ee180a8ff31ef4dfd49d8
adam-m52909t0h57knl0 <marchi.neves@gmail.com>
2026-01-30 15:20:44 +0100

    sim2.2

--- Preprocess/exp1/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys, os, inspect
-
-_here      = os.path.dirname(
-    os.path.abspath(inspect.getfile(inspect.currentframe())))
-M_PP_S1_EXP_DIR = os.path.join(_here)
-
-__all__ = [
-    'M_PP_S1_EXP_DIR',]
--- Preprocess/exp2/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys, os, inspect
-
-_here      = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-M_PP_S2_EXP_DIR = os.path.join(_here)
-
-__all__ = [
-    'M_PP_S2_EXP_DIR',]
